085eab7febe8be6d5cccca83334d238940b99756
zhangxd1222 <zhangxd1222@chaqianma.com>
2015-07-26 17:04:15 +0800

    --添加布局文件 --添加百度地图  --添加时间选择器 --修改样式

--- app/src/main/java/com/jwy/jd/utils/ResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package com.jwy.jd.utils;
-
-/**
- * Created by zhangxd on 2015/7/15.
- */
-public abstract class ResponseHandler<T> {
-    public abstract void onSuccess(T t);
-    public  void onFailure(String data) {}
-}
