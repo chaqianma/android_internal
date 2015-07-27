66185852328d08917587e02f433ec5da2468eed5
zhangxd1222 <zhangxd1222@chaqianma.com>
2015-07-27 17:43:53 +0800

    --界面样式调整  整个项目连接起来

---
+++ app/src/main/java/com/chaqianma/jd/common/AppData.java
@@ -0,0 +1,43 @@
+package com.chaqianma.jd.common;
+
+import com.chaqianma.jd.model.LocationInfo;
+import com.chaqianma.jd.model.UserInfo;
+
+/**
+ * Created by zhangxd on 2015/7/17.
+ */
+public class AppData {
+    private static AppData appData = null;
+    private UserInfo userInfo = null;
+
+    private LocationInfo locationInfo = null;
+
+    private AppData() {
+    }
+
+    public static AppData getInstance() {
+        if (appData == null) {
+            synchronized (AppData.class) {
+                if (appData == null)
+                    appData = new AppData();
+            }
+        }
+        return appData;
+    }
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    public LocationInfo getLocationInfo() {
+        return locationInfo;
+    }
+
+    public void setLocationInfo(LocationInfo locationInfo) {
+        this.locationInfo = locationInfo;
+    }
+}
---
+++ app/src/main/java/com/chaqianma/jd/common/HttpRequestURL.java
@@ -0,0 +1,15 @@
+package com.chaqianma.jd.common;
+
+/**
+ * Created by zhangxd on 2015/7/15.
+ * 网络请求链接常量
+ */
+public class HttpRequestURL {
+
+    //登录 POST
+    public static final String LoginUrl="api/user/login";
+    //尽调申请(贷款)
+    public static final String loanApplyUrl="api/borrow/dealing";
+    //修改密码
+    public static final String updatePasswordUrl="api/user/changepassword";
+}
